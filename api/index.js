a84d9a02347c277ace7371144feae93bdcfc4913
EJ <38365682+e-rojas@users.noreply.github.com>
2020-05-04 11:25:42 -0300

    update
    succes strip transactions

--- api/index.js
+++ api/index.js
@@ -13,7 +13,40 @@ app.get("/", (req, res) => {
 });
 
 app.post("/checkout", async (req, res) => {
-  console.log(req.body);
+  let error;
+  let status;
+  const { items, token, price } = req.body;
+
+  try {
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+    const charge = await stripe.charges.create({
+      amount: parseFloat(price) * 100,
+      currency: "cad",
+      customer: customer.id,
+      receipt_email: token.email,
+      description: `Purchased the McDonalds Meal`,
+
+      shipping: {
+        name: token.card.name,
+        address: {
+          line1: token.card.address_line1,
+          line2: token.card.address_line2,
+          city: token.card.address_city,
+          country: token.card.address_country,
+          postal_code: token.card.address_zip,
+        },
+      },
+    });
+
+    status = "success";
+  } catch (error) {
+    console.log("Error:", error);
+    status = "failure";
+  }
+  res.json({ error, status });
 });
 
 app.listen(4000, () => {
